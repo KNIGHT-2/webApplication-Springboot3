4899824a1c0e85802612eb4f3ec9c6c242336642
Patrick M <patrickmacedo02@gmail.com>
2024-02-28 11:38:30 -0300

    Service layer, component registration

--- src/main/java/com/patrick/application/resources/UserResource.java
+++ src/main/java/com/patrick/application/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.patrick.application.resources;
 
 import com.patrick.application.entities.User;
+import com.patrick.application.repositories.UserRepository;
+import com.patrick.application.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController //Dizendo que a classe será um controlador
 @RequestMapping(value = "/users")//dando o nome que será utilizado na requisição
 public class UserResource {
 
+    @Autowired
+    UserService service;
+
     @GetMapping//Dizendo que esse será o método responsável pelas requisições GET
-    public ResponseEntity<User> findAll(){
-        User obj = new User(1L, "Cleiton", "cleiton@email.com", "999999", "123");
-        return ResponseEntity.ok().body(obj);//Fazendo o "obj" ser retornado como corpo da requisição
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);//Fazendo o "obj" ser retornado como corpo da requisição
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById (@PathVariable Long id){
+
+        User obj = service.findById(id).orElseThrow(() -> new RuntimeException());
+        return ResponseEntity.ok().body(obj);
+    }
 }
---
+++ src/main/java/com/patrick/application/services/UserService.java
@@ -0,0 +1,25 @@
+package com.patrick.application.services;
+
+import com.patrick.application.entities.User;
+import com.patrick.application.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service//Registrando a classe como um "service"
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id){
+        return userRepository.findById(id);
+    }
+}
