b29285cdc6de19c90b5bdaff18944db6c120fc62
Patrick M <patrickmacedo02@gmail.com>
2024-03-18 16:25:41 -0300

    Exception handling - delete

--- src/main/java/com/patrick/application/services/UserService.java
+++ src/main/java/com/patrick/application/services/UserService.java
@@ -1,15 +1,16 @@
 package com.patrick.application.services;
 
-import com.patrick.application.entities.User;
-import com.patrick.application.repositories.UserRepository;
-import com.patrick.application.services.exceptions.ResourceNotFoundException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-import java.util.Optional;
+import com.patrick.application.entities.User;
+import com.patrick.application.repositories.UserRepository;
+import com.patrick.application.services.exceptions.DatabaseException;
+import com.patrick.application.services.exceptions.ResourceNotFoundException;
 
 @Service//Registrando a classe como um "service"
 public class UserService {
@@ -30,7 +31,14 @@ public class UserService {
     }
     
     public void delete(Long id) {
-    	userRepository.deleteById(id);
+    	try {
+    		userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+    	
     }
     
     public User update(Long id, User obj) {
